61300ad20aca303b42c7a63785774d116139288b
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-07-13 15:37:38 +0500

    save

---
+++ StudentGrades.Domain/Commons/BaseAuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentGrades.Domain.Commons
+{
+    internal class BaseAuditableEntity
+    {
+    }
+}
---
+++ StudentGrades.Domain/Entities/Subject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentGrades.Domain.Commons;
+
+namespace StudentGrades.Domain.Entities
+{
+    public class Subject : BaseAuditableEntity
+    {
+        public string SubjectName { get; set; }
+
+        public Guid TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+
+        public virtual ICollection<Grade> Grades { get; set; }
+    }
+}
